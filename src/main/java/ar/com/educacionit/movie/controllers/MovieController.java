a0f9cf90888e94d3b1e20b2d2018726c006c0a34
Maite Arakaki <maite.arakaki@gmail.com>
2024-02-11 20:52:12 -0500

    creado movie service y actualizado el movie controller

--- src/main/java/ar/com/educacionit/movie/controllers/MovieController.java
+++ src/main/java/ar/com/educacionit/movie/controllers/MovieController.java
@@ -3,23 +3,51 @@ package ar.com.educacionit.movie.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ar.com.educacionit.movie.domain.Movie;
 import ar.com.educacionit.movie.repository.MovieRepository;
+import ar.com.educacionit.movie.services.MovieService;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
 	
-	@Autowired
-	private MovieRepository repository;
+	//@Autowired
+	private final MovieService movieService;
+	
+	public MovieController (MovieService movieService) {
+		this.movieService = movieService;
+	}
 	
 	@GetMapping
 	public List<Movie> findAllMovies() {
-		List<Movie> movies =repository.findAll();
+		List<Movie> movies =movieService.findAll();
 		return movies;
 	}
+	
+	//una pelicula dada su id
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Movie> getById(
+			@PathVariable("id") Long id
+			){
+		
+		Movie movie =movieService.getById(id);
+		
+		if (movie == null ) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(movieService.getById(id)) ;
+	}
+	//crear una pelicula
+	
+	//eliminar una pelicula
+	
+	//actualizar una pelicula
+	
 }
---
+++ src/main/java/ar/com/educacionit/movie/services/MovieService.java
@@ -0,0 +1,26 @@
+package ar.com.educacionit.movie.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import ar.com.educacionit.movie.domain.Movie;
+import ar.com.educacionit.movie.repository.MovieRepository;
+
+@Service
+public class MovieService {
+	private final MovieRepository movieRepository;
+	
+	public MovieService(MovieRepository movieRepository) {
+		this.movieRepository = movieRepository;
+	}
+	
+	public List<Movie> findAll() {
+		return this.movieRepository.findAll();
+	}
+	
+	public Movie getById(Long id) {
+		return this.movieRepository.findById(id).orElse(null);
+	}
+
+}
